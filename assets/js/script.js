8eae3a246d8e7a465b17eb5d278222cb9ca140f0
GustavoOmai <omai.gustavoh@gmail.com>
2023-03-26 21:00:51 -0300

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+let checkbox = document.getElementById('checkbox-menu')
+let menu = document.getElementById('nav-mob')
+
+function hamburguer() {
+  if (checkbox.checked) {
+    menu.classList.remove('display-none')
+  } else {
+    menu.classList.add('display-none')
+  }
+}
+
+var scene = document.getElementById('scene');
+const parallaxInstance = new Parallax(scene);
+parallaxInstance.invert(false, false)
+parallaxInstance.scalar(30, 30)
+
+
+
+var hope = document.getElementById('hope');
+const hopepara = new Parallax(hope);
+hopepara.invert(false, false)
+hopepara.scalar(10, 10)
